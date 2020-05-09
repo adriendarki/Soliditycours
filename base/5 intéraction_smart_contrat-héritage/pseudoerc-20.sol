68d1ddd74fe351f2edd70d42813ac22057cc4c4a
adrien darki <adriendarki@outlook.fr>
2020-05-09 19:30:12 +0200

    ajout intéraction smart contrat

---
+++ base/5 intéraction_smart_contrat-héritage/pseudoerc-20.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.6.4;
+
+contract ERC20Token {
+    string public name;
+    mapping(address => uint256) public balances;
+
+    constructor(string memory _name) public {
+        name = _name;
+    }
+
+    function mint() public virtual {
+        balances[tx.origin] ++;
+    }
+}
+
+contract MyToken is ERC20Token {
+    string public symbol;
+    address[] public owners;
+    uint256 public ownerCount;
+
+    constructor(
+        string memory _name,
+        string memory _symbol
+    )
+        ERC20Token(_name)
+    public {
+        symbol = _symbol;
+    }
+
+    function mint() public override {
+        super.mint();
+        ownerCount ++;
+        owners.push(msg.sender);
+    }
+
+}
