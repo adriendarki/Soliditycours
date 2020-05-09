9879755d30b1ae61079031de2bf3fda087928cc5
adrien darki <adriendarki@outlook.fr>
2020-05-09 12:10:40 +0200

    ajout cours

---
+++ base/3 Function_visible-mofication-temps/firstanameandlastname.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.6.4;
+
+contract NameAll {
+    uint256  public peopleCount = 0;
+
+    mapping(uint => Person) public people;
+
+    uint256 openingTime = 1544613571;
+
+    modifier onlyWhileOpen() {
+        require(block.timestamp >= openingTime);
+        _;
+    }
+
+    struct Person {
+        uint id;
+        string _fristName;
+        string _lastName;
+    }
+
+
+    function addPerson(
+    string memory _firstName,
+    string memory _lastName
+    )
+    public onlyWhileOpen
+      {
+        incrementCount();
+        people[peopleCount] = Person(peopleCount, _firstName, _lastName);
+    }
+
+    function incrementCount() internal {
+        peopleCount += 1;
+    }
+}
