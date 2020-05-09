5128a58174e9a016993e906e5295225d14e603a7
adrien darki <adriendarki@outlook.fr>
2020-05-09 14:46:45 +0200

    transaction et evenement

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-Les bases de solidity
+# Cour sur  solidity
+
+
+si vous avez pas IDE utilisée https://remix.ethereum.org/ pour suivre les réalisation de puis votre pc
+
+un IDE sera oubligatoire des que j'utiliserais web3.js | truffle.js et ganache
--- base/4 transaction-evenements/transaction.sol
+++ base/4 transaction-evenements/transaction.sol
@@ -2,5 +2,26 @@ pragma solidity ^0.6.4;
 
 contract Transaction {
 
-    function buyToken()
+    mapping(address => uint256) public balances;
+    address payable  wallet;
+
+    event Purchase(
+        address indexed _buyer,
+        uint256 _amount
+    );
+
+    constructor(address payable _wallet) public {
+        wallet = _wallet;
+    }
+
+    receive() external payable {
+        buyToken();
+    }
+
+    function buyToken()public payable{
+        balances[msg.sender] += 1;
+        wallet.transfer(msg.value);
+        emit Purchase(msg.sender, 1);
+
+    }
 }
